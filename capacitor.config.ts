8c3821c3ec0f208d4f82653651122f8d6aa41521
DonYosef <josi.munoz@duocuc.cl>
2023-11-11 09:13:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jose_munnoz_prueba2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
